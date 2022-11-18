470418de0a27cc50dcb09cbd881ddd147af0ef27
msmallwood20 <72398741+msmallwood20@users.noreply.github.com>
2022-11-17 19:11:28 -0500

    Tik-Tac-Toe C# Game
    You look through the code to get an idea of what we can do with the Card Game, planning to switch to making it a 2D array.

---
+++ Tik-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace _7_q8_MNS_11_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewGamebutton = new System.Windows.Forms.Button();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            this.Winnerlabel = new System.Windows.Forms.Label();
+            this.C1R1 = new System.Windows.Forms.Label();
+            this.C2R1 = new System.Windows.Forms.Label();
+            this.C3R1 = new System.Windows.Forms.Label();
+            this.C1R2 = new System.Windows.Forms.Label();
+            this.C2R2 = new System.Windows.Forms.Label();
+            this.C3R2 = new System.Windows.Forms.Label();
+            this.C1R3 = new System.Windows.Forms.Label();
+            this.C2R3 = new System.Windows.Forms.Label();
+            this.C3R3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NewGamebutton
+            // 
+            this.NewGamebutton.AutoSize = true;
+            this.NewGamebutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewGamebutton.Location = new System.Drawing.Point(285, 330);
+            this.NewGamebutton.Name = "NewGamebutton";
+            this.NewGamebutton.Size = new System.Drawing.Size(78, 23);
+            this.NewGamebutton.TabIndex = 0;
+            this.NewGamebutton.Text = "New Game";
+            this.NewGamebutton.UseVisualStyleBackColor = true;
+            this.NewGamebutton.Click += new System.EventHandler(this.NewGamebutton_Click);
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exitbutton.Location = new System.Drawing.Point(392, 330);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(75, 23);
+            this.Exitbutton.TabIndex = 1;
+            this.Exitbutton.Text = "Exit";
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            this.Exitbutton.Click += new System.EventHandler(this.Exitbutton_Click);
+            // 
+            // Winnerlabel
+            // 
+            this.Winnerlabel.AutoSize = true;
+            this.Winnerlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Winnerlabel.Location = new System.Drawing.Point(236, 293);
+            this.Winnerlabel.MinimumSize = new System.Drawing.Size(300, 20);
+            this.Winnerlabel.Name = "Winnerlabel";
+            this.Winnerlabel.Size = new System.Drawing.Size(300, 20);
+            this.Winnerlabel.TabIndex = 2;
+            this.Winnerlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C1R1
+            // 
+            this.C1R1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C1R1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1R1.Location = new System.Drawing.Point(236, 45);
+            this.C1R1.Name = "C1R1";
+            this.C1R1.Size = new System.Drawing.Size(69, 60);
+            this.C1R1.TabIndex = 3;
+            this.C1R1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C2R1
+            // 
+            this.C2R1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C2R1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2R1.Location = new System.Drawing.Point(352, 45);
+            this.C2R1.Name = "C2R1";
+            this.C2R1.Size = new System.Drawing.Size(69, 60);
+            this.C2R1.TabIndex = 3;
+            this.C2R1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C3R1
+            // 
+            this.C3R1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C3R1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3R1.Location = new System.Drawing.Point(467, 45);
+            this.C3R1.Name = "C3R1";
+            this.C3R1.Size = new System.Drawing.Size(69, 60);
+            this.C3R1.TabIndex = 3;
+            this.C3R1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C1R2
+            // 
+            this.C1R2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C1R2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1R2.Location = new System.Drawing.Point(236, 131);
+            this.C1R2.Name = "C1R2";
+            this.C1R2.Size = new System.Drawing.Size(69, 60);
+            this.C1R2.TabIndex = 3;
+            this.C1R2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C2R2
+            // 
+            this.C2R2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C2R2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2R2.Location = new System.Drawing.Point(352, 131);
+            this.C2R2.Name = "C2R2";
+            this.C2R2.Size = new System.Drawing.Size(69, 60);
+            this.C2R2.TabIndex = 3;
+            this.C2R2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C3R2
+            // 
+            this.C3R2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C3R2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3R2.Location = new System.Drawing.Point(467, 131);
+            this.C3R2.Name = "C3R2";
+            this.C3R2.Size = new System.Drawing.Size(69, 60);
+            this.C3R2.TabIndex = 3;
+            this.C3R2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C1R3
+            // 
+            this.C1R3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C1R3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1R3.Location = new System.Drawing.Point(236, 219);
+            this.C1R3.Name = "C1R3";
+            this.C1R3.Size = new System.Drawing.Size(69, 60);
+            this.C1R3.TabIndex = 3;
+            this.C1R3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C2R3
+            // 
+            this.C2R3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C2R3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2R3.Location = new System.Drawing.Point(352, 219);
+            this.C2R3.Name = "C2R3";
+            this.C2R3.Size = new System.Drawing.Size(69, 60);
+            this.C2R3.TabIndex = 3;
+            this.C2R3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // C3R3
+            // 
+            this.C3R3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.C3R3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3R3.Location = new System.Drawing.Point(467, 219);
+            this.C3R3.Name = "C3R3";
+            this.C3R3.Size = new System.Drawing.Size(69, 60);
+            this.C3R3.TabIndex = 3;
+            this.C3R3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.C3R3);
+            this.Controls.Add(this.C3R2);
+            this.Controls.Add(this.C3R1);
+            this.Controls.Add(this.C2R3);
+            this.Controls.Add(this.C2R2);
+            this.Controls.Add(this.C2R1);
+            this.Controls.Add(this.C1R3);
+            this.Controls.Add(this.C1R2);
+            this.Controls.Add(this.C1R1);
+            this.Controls.Add(this.Winnerlabel);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.NewGamebutton);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewGamebutton;
+        private System.Windows.Forms.Button Exitbutton;
+        private System.Windows.Forms.Label Winnerlabel;
+        private System.Windows.Forms.Label C1R1;
+        private System.Windows.Forms.Label C2R1;
+        private System.Windows.Forms.Label C3R1;
+        private System.Windows.Forms.Label C1R2;
+        private System.Windows.Forms.Label C2R2;
+        private System.Windows.Forms.Label C3R2;
+        private System.Windows.Forms.Label C1R3;
+        private System.Windows.Forms.Label C2R3;
+        private System.Windows.Forms.Label C3R3;
+    }
+}
+
