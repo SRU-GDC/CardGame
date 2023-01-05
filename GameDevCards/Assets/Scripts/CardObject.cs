afb4246f9a55e680a37b3b878d02cab184a2705b
msmallwood20 <mayasmallwood41@gmail.com>
2023-01-04 21:06:19 -0500

    1.4.22 Code & Game Update
    1. User can only flip card once (now)
    2. Flipped card sprite is stored with an index
    3. Index of sprite number from an array is stored into a board array
    4.Board array now matches along with the cardObject index
    5.Game now automatically draws all 9 cards to game
    6.Re-documnted some code
    7.Hide some debugging buttons from game view mode
    8.7.To be done next is search through board array and check for matches

--- GameDevCards/Assets/Scripts/CardObject.cs
+++ GameDevCards/Assets/Scripts/CardObject.cs
@@ -22,12 +22,10 @@ public class CardObject : MonoBehaviour
     public new string name;
     //Game Level speficfices what level the card will show on 
     public string category;
-    //Background of the card, what it shown to the player before they select it
-    public Sprite BG_Back;
     //Front of card, that will show when the play selects it, will be pulling from an array
     public Sprite[] IconArray;
-    //Index measurement of array
-    public int arrayIndex;
+    //Index value of IconArray
+    public int iconArraryIndex;
     //Hints to the player on where a matching set is, rare popups or unlock
     public string hints;
     //Reference to Sprite render
@@ -42,8 +40,6 @@ public class CardObject : MonoBehaviour
         gm = FindObjectOfType<GameManger>();
         //Sets the spriteRender and referencing to gameobject and inspector
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        //Sets card image from start-Card Background
-        spriteRenderer.sprite = BG_Back;
     }
 
     //Calls this method whenever a card is pressed
@@ -56,28 +52,13 @@ public class CardObject : MonoBehaviour
         }
     }
 
-    /*private void OnMouseDown()
-    {
-        
-        if (!locatedinDeck)
-        {
-            //Postions card onto card slot
-            Instantiate(BG_Back, transform.position, Quaternion.identity);
-            locatedinDeck = true;
-            gm.availableCardSlots[handIndex] = true;
-            
-        }
-    }*/
-
     //Changes the sprite design on the card
     public void ChangeSprite()
     {
         //Checks if card is flipped to show graphic
         flipped = true;
-        //Assigns for a list of graphics to randomly to card
-        arrayIndex = Random.Range(0, IconArray.Length);
         //Takes theat choosen graphic and changes card image
-        spriteRenderer.sprite = IconArray[arrayIndex];
+        spriteRenderer.sprite = IconArray[iconArraryIndex];
     }
 
 }
--- GameDevCards/Assets/Scripts/GameManger.cs
+++ GameDevCards/Assets/Scripts/GameManger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 public class GameManger : MonoBehaviour
 {
@@ -12,13 +13,27 @@ public class GameManger : MonoBehaviour
     //Variables
     //Ui Text on cavnas
     public TextMeshProUGUI deckSizeCount;
-    
-    //List will contain all cards in the deck
+
+    //List will contain all cards objects in the deck
     public List<CardObject> deck;
     //Array holds cardsSlots
     public Transform[] cardSlots;
     //Check how many slots can be filled/need filled
     public bool[] availableCardSlots;
+    //Arrary board-Cards that are placed on the board
+    public int[] board;
+    public int boardsize = 0;
+    public int setBoard;
+
+    //Starts at the start of the project exctution
+    //Runs DrawCard method to setup board
+    private void Start()
+    {
+        for (setBoard = 0; setBoard < 9; setBoard++)
+        {
+            DrawCard();
+        }
+    }
 
     //Update's game per frame rate
     private void Update()
@@ -29,9 +44,11 @@ public class GameManger : MonoBehaviour
     //Method to Draw out cards and place them in any avilable Card Slot
     public void DrawCard()
     {
-        //Check if any cards are avaiable before adding in a random card
+
+        //Check if any card objects are avaiable before placing cards on to the board
         if (deck.Count >= 1)
         {
+            //Cycles through deck size and assigns slot
             for (int i = 0; i < deck.Count; i++)
             {
                 cardObject = deck[i];
@@ -43,26 +60,50 @@ public class GameManger : MonoBehaviour
                     {
                         //Grabs the given random card
                         cardObject.gameObject.SetActive(true);
-                        cardObject.handIndex = j;
                         //Places that card in one of the card Slots avaiable
                         cardObject.transform.position = cardSlots[j].position;
                         cardObject.locatedinDeck = false;
-                        deck.Remove(cardObject);
                         //Takes that card slot setting it to false to make sure it's not overwrited by another card
                         availableCardSlots[j] = false;
+                        //Assigns for a list of graphics to randomly to card
+                        cardObject.iconArraryIndex = Random.Range(0, cardObject.IconArray.Length);
+                        //Adds sprite Icon Index from card to array to the board array
+                        board[boardsize] = cardObject.iconArraryIndex;
+                        //Move to next element in array to store iconIndex
+                        boardsize++;
+                        //Removes cardObject from deck to move to next card
+                        deck.Remove(cardObject);
                         return;
                     }
                 }
             }
         }
     }
-        //Checks what sprite is in card
-        public void CheckSlot()
+    //Checks what sprite is in card-Debugging
+    public void CheckSlot()
     {
-        for (int i = 0; deck.Count > 0; i++)
+        if (cardObject.flipped)
         {
-            Debug.Log(cardObject.IconArray[i]);
+            Debug.Log(cardObject.spriteRenderer.sprite);
+            //board[0] = cardObject.spriteRenderer.sprite;
+        }
+        else
+        {
+            Debug.Log("Flip Cards to see what's under them.");
         }
     }
+    
+    //Returns values in the board-Debugging
+    public void CheckBoad()
+    {
+        for (int i = 0; i <board.Length; i++)
+        {
+            Debug.Log(board[i]);
+        }
+    }
+
+    //Checking if any spots in the board are matches & Checking if there are no matches at all
+    //Row 1: 0,1,2 Row 2: 3,4,5 Row 3: 6,7,8 
+    //Columns 1: 0,3,6 Columns 2: 1,4,7 Columns 3: 2,5,8
 
 }
