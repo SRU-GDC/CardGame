e38deacca5203a38dfe8c6359a6cf77ae930cb23
msmallwood20 <mayasmallwood41@gmail.com>
2023-01-06 15:26:21 -0500

    1.6.22 Matches & Reset
    Game can now check rows for matches and returns if a win or lose is found, of no matches resets game for player to retry.

--- GameDevCards/Assets/Scripts/GameManger.cs
+++ GameDevCards/Assets/Scripts/GameManger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class GameManger : MonoBehaviour
 {
@@ -24,6 +25,7 @@ public class GameManger : MonoBehaviour
     public int[] board;
     public int boardsize = 0;
     public int setBoard;
+    public bool Win = false;
 
     //Starts at the start of the project exctution
     //Runs DrawCard method to setup board
@@ -96,14 +98,77 @@ public class GameManger : MonoBehaviour
     //Returns values in the board-Debugging
     public void CheckBoad()
     {
-        for (int i = 0; i <board.Length; i++)
+        /*Debugging
+         * for (int i = 0; i <board.Length; i++)
         {
             Debug.Log(board[i]);
+        }*/
+        //Checking if any spots in the board are matches & Checking if there are no matches at all
+        //Row 1: 0,1,2
+        if (board[0] == 0 && board[1] == 0 && board[2] == 0)
+            {
+                Debug.Log("You Win!");
+            }
+                else if(board[0] == 1 && board[1] == 1 && board[2] == 1)
+                {
+                    Debug.Log("You Win!");
+                }
+                    else if (board[0] == 2 && board[1] == 2 && board[2] == 2)
+                    {
+                        Debug.Log("You Win!");
+                     }
+        //Row 2: 3,4,5
+        if (board[3] == 0 && board[4] == 0 && board[5] == 0)
+        {
+            Win = true;
+            Debug.Log("You Win!");
         }
+        else if (board[3] == 1 && board[4] == 1 && board[5] == 1)
+        {
+            Win = true;
+            Debug.Log("You Win!");
+        }
+        else if (board[3] == 2 && board[4] == 2 && board[5] == 2)
+        {
+            Win = true;
+            Debug.Log("You Win!");
+        }
+        //Row 3: 6,7,8 
+        else if (board[6] == 0 && board[7] == 0 && board[8] == 0)
+        {
+            Win = true;
+            Debug.Log("You Win!");
+        }
+        else if (board[6] == 1 && board[7] == 1 && board[8] == 1)
+        {
+            Win = true;
+            Debug.Log("You Win!");
+        }
+        else if (board[6] == 2 && board[7] == 2 && board[8] == 2)
+        {
+            Win = true;
+            Debug.Log("You Win!");
+        }
+        else if (Win == false)
+        {
+            Debug.Log("You didn't find any matches, reseting Game");
+            ResetGame();
+        }
+
+
+
+        //Columns 1: 0,3,6
+
+        //Columns 2: 1,4,7
+
+        //Columns 3: 2,5,8
+
+    }
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
     }
 
-    //Checking if any spots in the board are matches & Checking if there are no matches at all
-    //Row 1: 0,1,2 Row 2: 3,4,5 Row 3: 6,7,8 
-    //Columns 1: 0,3,6 Columns 2: 1,4,7 Columns 3: 2,5,8
 
 }
